96e271998632ad7d9cfdc49b80aac2d4ffa80d48
younis <ali1986111@gmail.com>
2024-07-16 13:23:46 +0300

    Add progress bar, Enhance storing

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from telethon import TelegramClient
 import asyncio
+import sys
+import config
+import re
 
-client = TelegramClient('session', 123456, "YOUR_API_HASH", device_model='iPhone X')
+
+client = TelegramClient('session', config.api_id, config.api_hash, device_model='iPhone X')
 
 options = [
     "1: Show current chats",
     "2: Close",
 ]
+
+
+def progress(received_bytes, total):
+    percentage = received_bytes / total
+    sys.stdout.write('\r')
+    sys.stdout.write(f"[{'=' * int(25 * percentage):{25}s}] {round((received_bytes / 10**6) * 100 ) / 100} MB [{int(100 * percentage)}%] ")
+    sys.stdout.flush()
+
+
+def check_name(name):
+    folder_name = re.sub(r'[^a-zA-Z]', '', name)
+    if not folder_name:
+        folder_name = "New_Folder"
+    return folder_name
 
 
 async def download_media(chosen_dialog):
@@ -15,14 +33,14 @@ async def download_media(chosen_dialog):
     async for message in client.iter_messages(chosen_dialog.id, None):
         photo = message.photo
         if photo:
-            org_filename = chosen_dialog.name + '_' + str(message.id) + '.jpg'
-            photo_path = await client.download_media(photo, file='self/' + org_filename)
-            print(f"Photo saved to: {photo_path}")
+            org_filename = 'Downloads/' + check_name(chosen_dialog.name) + '/' + str(message.id) + '.jpg'
+            photo_path = await client.download_media(photo, file=org_filename, progress_callback=progress)
+            print(f" Photo saved to: {photo_path}")
             photos_count += 1
         elif message.video:
-            org_filename = chosen_dialog.name + '_' + str(message.id) + '.mp4'
-            video_path = await client.download_media(message.video, file='self/' + org_filename)
-            print(f"Video saved to: {video_path}")
+            org_filename = 'Downloads/' + check_name(chosen_dialog.name) + '/' + str(message.id) + '.mp4'
+            video_path = await client.download_media(message.video, file=org_filename, progress_callback=progress)
+            print(f" Video saved to: {video_path}")
             video_count += 1
     print(f"Successfully saved: {photos_count} photos")
     print(f"Successfully saved: {video_count} videos")
@@ -37,9 +55,9 @@ async def show_options():
         dialogs = await client.get_dialogs()
         i = 1
         for dialog in dialogs:
-            print(i, dialog.name, '(', dialog.id, ')')
+            print(i, dialog.name)
             i += 1
-        choice = input("Choose a chat to download self destructing photo inside it: ")
+        choice = input("Choose a chat to start download media: ")
         choice = int(choice)
         chosen_dialog = dialogs[choice - 1]
         await download_media(chosen_dialog)
